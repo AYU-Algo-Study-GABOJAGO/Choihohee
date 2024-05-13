74d52dc57c31b9f6797c01a6075d070be364da56
Choihohee <lovelove4927@naver.com>
2024-05-13 22:20:18 +0900

    add: Baekjoon12845

---
+++ src/week06/Baekjoon12845.java
@@ -0,0 +1,30 @@
+package week06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon12845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        String arr[] = br.readLine().split(" ");
+
+        int L;
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            L = Integer.parseInt(arr[i]);
+            sum += L;
+
+            if (max < L) {
+                max = L;
+            }
+        }
+        System.out.println(sum + (max * (N - 2)));
+    }
+}
